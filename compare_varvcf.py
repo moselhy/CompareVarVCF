4b18b7935314cc01c4ab1d67c4624ac065ca9db0
Mohamed Moselhy <mohamed.moselhy@uwo.ca>
2018-06-26 14:34:04 +0200

    Now the script compares the actual entries rather than just the variant position, more accurate stats

--- compare_varvcf.py
+++ compare_varvcf.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+""" SCRIPT TO COMPARE OUTPUT FILE FROM UPVC 1.3 TO .VAR FILES
+Created by: Dr. Dominique Lavenier, INRIA, Rennes
+Last modified by: Mohamed Moselhy, UWO, Canada
+ """
+
 import sys
 
 # Data in .var file
@@ -10,8 +15,8 @@ while l!= "":
     ll = l.split()
     # Get chromosome_position
     ident = ll[0]+"_"+ll[1]
-    # Get the whole entry except the last character (new line)
-    DVAR[ident] = l[:-1]
+    # Get the type of substitution and variant
+    DVAR[ident] = ll[2]+" "+ll[-2]
     l = fvar.readline()
 fvar.close()
 
@@ -24,12 +29,12 @@ while l!= "":
     # Get qualities of variant
     q0 = int(ll[4][1:-1].split('/')[0])
     q1 = int(ll[4][1:-1].split('/')[1])
-	# If the quality meets threshold, add it to the dataset to compare
+    # If the quality meets threshold, add it to the dataset to compare
     if q0 >= 5 and q1 < 200:
-    	# Get chromosome_position
+        # Get chromosome_position
         ident = ll[0]+"_"+ll[1]
-        # Get the whole entry except the new line character
-        DVCF[ident] = l[:-1]
+        # Get the type of substitution and variant
+        DVCF[ident] = ll[2]+" "+ll[3]
     l = fvcf.readline()
 fvcf.close()
 
@@ -43,26 +48,26 @@ delta = 20
 TP = 0
 FN = 0
 for x in DVAR:
-	# Get the chromosome number
+    # Get the chromosome number
     ns = int(x.split('_')[0])
     # Get the variant position
     ix = int(x.split('_')[1])
     ok = False
     # Iterate through other file's variants until a match is found or until lee-way is reached
-    for i in range(ix-delta,ix+delta):
+    for i in range(ix-delta,ix+delta+1):
         ident = str(ns)+"_"+str(i)
-        # If a variant is found in the other file, then stop there
-        if ident in DVCF:
+        # If the same variant is found in the other file, then stop there
+        if ident in DVCF and DVCF[ident] == DVAR[x]:
             ok = True
             break
-    # If variant in that position is found, count a true positive
+    # check if true positive
     if ok == True:
         TP = TP+1
-        ftp.write(DVAR[x]+"\n")
+        ftp.write(x.replace('_', ' ') + ' ' + DVAR[x]+"\n")
     # Otherwise, if no variant is found in the VCF file, count a false negative
     else:
         FN = FN+1
-        ffn.write(DVAR[x]+"\n")
+        ffn.write(x.replace('_', ' ') + ' ' + DVAR[x]+"\n")
 ftp.close()
 ffn.close()
 
@@ -76,12 +81,12 @@ for x in DVCF:
     ok = False
     for i in range(ix-delta,ix+delta):
         ident = str(ns)+"_"+str(i)
-        if ident in DVAR:
+        if ident in DVAR and DVAR[ident] == DVCF[x]:
             ok = True
             break
     if ok == False:
         FP = FP+1
-        ffp.write(DVCF[x]+"\n")
+        ffp.write(x.replace('_', ' ') + ' ' + DVCF[x]+"\n")
 ffp.close()
 
 # Output results to file
